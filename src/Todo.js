da2406a4de4439910c18b58df01f955757e4a98c
Gabriel <gabriel.alves.rosario@gmail.com>
2022-12-14 16:59:04 +0000

    primeiroCommit

---
+++ src/Todo.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react"
+import "./Todo.css"
+
+function Todo(){
+
+    const [text, setText] = useState("")
+
+    function handleChanger(event){
+        let t = event.target.value
+        setText(t)
+    }
+    return (
+    <div className="container">
+        <h1>Todo</h1>
+        <form>
+            <input type="text" onChange={handleChanger}></input>
+            <button>Add</button>
+        </form>
+        <ul>
+            <li>{text}</li>
+        </ul>
+    </div>
+    )
+}
+
+export default Todo
