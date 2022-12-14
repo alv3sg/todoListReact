6ec8b87bedd145a09d6d4562c13fcefc37fb2034
Gabriel <gabriel.alves.rosario@gmail.com>
2022-12-14 18:10:05 +0000

    deletandoItens

--- src/List.js
+++ src/List.js
@@ -1,9 +1,13 @@
 import "./Todo.css"
 //recebdno um array de objetos, utilizando map para mapear e criar uma lista com seus itens
 function List(props){
+    
     return(
         <ul>
-            {props.items.map(item => <li key={item.id}>{item.text}</li>)}
+            {props.items.map(item => <li key={item.id}>
+                {item.text}
+                <button onClick={()=>{props.onItemDelleted(item)}}>DEL</button>
+            </li>)}
         </ul>
     )
 }
--- src/Todo.js
+++ src/Todo.js
@@ -8,16 +8,23 @@ function Todo(){
 
     function onAddItem(text){
         //receber estado de um filho atraves de uma funcao
-        let it = new Item(text)
+        let item = new Item(text)
         //recebendo um objt
-        setItems([...items, it])         
+        setItems([...items, item])         
+    }
+    function onItemDelleted(item){
+        //recebendo o item que vai ser deletado, salvando uma lista com todos os item menos o que sera deletado
+        let filteredItems = items.filter(it=>it.id != item.id)
+        //colocando os novos item na tela sem o item deletado
+        setItems(filteredItems)
+    
     }
     
     return (
     <div className="container">
         <h1>Todo</h1>
         <TodoForm onAddItem={onAddItem}></TodoForm>
-        <List items={items}></List>        
+        <List onItemDelleted={onItemDelleted} items={items}></List>        
     </div>
     )
 }
